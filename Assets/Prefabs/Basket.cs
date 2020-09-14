fe9a8ae385e828930919a1435078917090244a07
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-09-14 13:57:37 -0400

    Adding points code

--- Assets/Prefabs/Basket.cs
+++ Assets/Prefabs/Basket.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
-    private void Start()
+    [Header("Set Dynamically")]
+
+    public Text scoreGT;
+
+    void Start()
     {
+        //Find a reference to the ScoreCounter GameObject
+
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        //Get the Text Component of that GameObject
+
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        //Set the starting number of points to 0
+
+        scoreGT.text = "0";
         
     }
 
@@ -42,5 +58,17 @@ public class Basket : MonoBehaviour
         {
             Destroy(collidedWith);
         }
+
+        //Parse the text of the scoreGT into an int
+
+        int score = int.Parse(scoreGT.text);
+
+        //Add points for catching the apple
+
+        score += 100;
+
+        //Convert the score back to a string and display it
+
+        scoreGT.text = score.ToString();
     }
 }
